6c851eae5015b74ba818cb50bf18bf25716e19ef
PaTham-bu <pannawit.tham@bumail.net>
2026-05-04 01:30:40 +0700

    UPD 07 - Create Finish line for each game scenes and Testing

---
+++ Assets/Script/Scene/FinishFlag.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishFlag : MonoBehaviour
+{
+    public int nextSceneIndex; // choose scene in Inspector
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LoadNext();
+        }
+    }
+
+    void LoadNext()
+    {
+        Time.timeScale = 1f; // important if paused
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
