13d54f08438351fae5f79ea38fcd95af87177196
PaTham-bu <pannawit.tham@bumail.net>
2026-05-04 01:57:56 +0700

    UPD 08 - Create Timer and Fix some UI code

--- Assets/Script/Scene/GameTimer.cs
+++ Assets/Script/Scene/GameTimer.cs
@@ -1,16 +1,64 @@
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameTimer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameTimer instance;
+
+    private TMP_Text timerText;
+
+    private float timeElapsed = 0f;
+    private bool isRunning = true;
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        TMP_Text[] texts = FindObjectsOfType<TMP_Text>();
+
+        foreach (var t in texts)
+        {
+            if (t.name == "TimerText")
+            {
+                timerText = t;
+                break;
+            }
+        }
+    }
+
     void Update()
     {
-        
+        if (!isRunning || timerText == null) return;
+
+        timeElapsed += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(timeElapsed / 60);
+        int seconds = Mathf.FloorToInt(timeElapsed % 60);
+
+        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+
+        Debug.Log(timerText);
     }
 }
